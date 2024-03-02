297ff242a9a85cfb22eee2da39e52362bf48f9bf
sbateca <sergio.bateca@ioet.com>
2024-03-02 18:59:35 -0500

    feat: covering test cases

---
+++ src/components/atoms/Spinner/Spinner.test.tsx
@@ -0,0 +1,11 @@
+import { render } from "@testing-library/react";
+import CircularSpinner from "./Spinner";
+
+
+describe("CircularSpinner", () => {
+  it("renders circular spinner", () => {
+    const { getByRole } = render(<CircularSpinner />);
+    const spinner = getByRole("progressbar");
+    expect(spinner).toBeInTheDocument();
+  });
+});
---
+++ src/components/atoms/TableCell/TableCell.test.tsx
@@ -0,0 +1,14 @@
+import { render, screen } from "@testing-library/react"
+import TableCellComponent from "./TableCell"
+
+describe("TableCell", ()=>{
+    it("TableCell renders the text passed as parameter", () => {
+        const mockText = "cell text";
+        const mockAlign = "center";
+
+        render(<TableCellComponent text={mockText} align={mockAlign} />);
+
+        const cell = screen.getByText(mockText);
+        expect(cell).toBeInTheDocument();
+    });
+});
