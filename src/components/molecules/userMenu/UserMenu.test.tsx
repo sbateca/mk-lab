773e8a4baeb893bd29a62c210bd4f76a060d17fe
sbateca <sergio.bateca@ioet.com>
2024-03-07 14:03:05 -0500

    feat: implement UserMenu tests

--- src/components/molecules/userMenu/UserMenu.test.tsx
+++ src/components/molecules/userMenu/UserMenu.test.tsx
@@ -25,12 +25,14 @@ describe("UserMenu component", () => {
 
   it("opens the user menu when IconButton is clicked", () => {
     fireEvent.click(screen.getByLabelText("account of current user"));
+
     expect(screen.getByText("Logout")).toBeInTheDocument();
   });
 
   it("calls cookies.remove and reloads the window when Logout is clicked", () => {
     fireEvent.click(screen.getByLabelText("account of current user"));
     fireEvent.click(screen.getByText("Logout"));
+
     expect(mockCookies.remove).toHaveBeenCalledWith("userData");
     expect(mockReload).toHaveBeenCalled();
   });
