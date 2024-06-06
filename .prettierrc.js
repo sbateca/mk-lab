927b846fba31777313bd1731e65c5ad7ed7785d7
sbateca <sergio.bateca@ioet.com>
2024-06-06 12:38:32 -0500

    feat: including esLint, prettier and gts libraries to follow google guide style for Typescript

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+	...require('gts/.prettierrc.json'),
+	arrowParens : "always",
+	endOfLine: 'auto',
+	semi: true,
+	trailingComma: 'all',
+	singleQuote: false
+  };
--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,11 @@
-export default {
-    preset: "ts-jest",
-    testEnvironment: "jest-environment-jsdom",
-    transform: {
-      "^.+\\.tsx?$": "ts-jest"
-    },
-    moduleNameMapper: {
-      "\\.(gif|ttf|eot|svg|png)$": "jest-transform-stub"
-    },
-    setupFilesAfterEnv: ["@testing-library/jest-dom"]
-  };
-  
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "jest-environment-jsdom",
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  moduleNameMapper: {
+    "\\.(gif|ttf|eot|svg|png)$": "jest-transform-stub",
+  },
+  setupFilesAfterEnv: ["@testing-library/jest-dom"],
+};
